d96f9aa16ab6787432e3c4d697f5417076ac2f08
Kaustubh Gharat <122547394+Kaustubh1504@users.noreply.github.com>
2023-02-11 23:56:45 +0530

    Create icimi_and_trinangle.cpp

---
+++ Maths/icimi_and_trinangle.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int t;
+	cin>>t;
+
+	while(t--){
+		long long a,b,c,d;
+		cin>>a>>b>>c>>d;
+		cout<<b<<" "<<c<<" "<<c<<endl;
+
+	}
+	return 0;
+}
